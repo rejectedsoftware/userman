956fe20df50dd97b3ea2e0910cd87b1540590c03
Sönke Ludwig <sludwig@outerproduct.org>
2020-06-07 21:19:42 +0200

    Move the (insecure!) password hash implementation into the library.
    This was removed from vibe.d and should be replaced here with a secure password hash function ASAP.

--- source/userman/db/controller.d
+++ source/userman/db/controller.d
@@ -320,14 +320,46 @@ string generateActivationCode()
 
 string generatePasswordHash(string password)
 @safe {
-	// TODO: use a more secure hash method
-	import vibe.crypto.passwordhash : generateSimplePasswordHash;
-	return () @trusted { return generateSimplePasswordHash(password); } ();
+	import std.base64 : Base64;
+
+	// FIXME: use a more secure hash method
+	ubyte[4] salt;
+	foreach( i; 0 .. 4 ) salt[i] = cast(ubyte)uniform(0, 256);
+	ubyte[16] hash = md5hash(salt, password);
+	return Base64.encode(salt ~ hash).idup;
 }
 
 bool validatePasswordHash(string password_hash, string password)
 @safe {
-	// TODO: use a more secure hash method
-	import vibe.crypto.passwordhash : testSimplePasswordHash;
-	return () @trusted { return testSimplePasswordHash(password_hash, password); } ();
+	import std.base64 : Base64;
+
+	// FIXME: use a more secure hash method
+	import std.string : format;
+	ubyte[] upass = Base64.decode(password_hash);
+	enforce(upass.length == 20, format("Invalid binary password hash length: %s", upass.length));
+	auto salt = upass[0 .. 4];
+	auto hashcmp = upass[4 .. 20];
+	ubyte[16] hash = md5hash(salt, password);
+	return hash == hashcmp;
+}
+
+private ubyte[16] md5hash(ubyte[] salt, string[] strs...)
+@safe {
+	static if( __traits(compiles, {import std.digest.md;}) ){
+		import std.digest.md;
+		MD5 ctx;
+		ctx.start();
+		ctx.put(salt);
+		foreach( s; strs ) ctx.put(cast(const(ubyte)[])s);
+		return ctx.finish();
+	} else {
+		import std.md5;
+		ubyte[16] hash;
+		MD5_CTX ctx;
+		ctx.start();
+		ctx.update(salt);
+		foreach( s; strs ) ctx.update(s);
+		ctx.finish(hash);
+		return hash;
+	}
 }
